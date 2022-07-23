0419e119be59a8ce884c9824e2e235404e5bda60
Takumikimura1899 <acmiran189926@gmail.com>
2022-07-23 20:19:24 +0900

    asyncData Hook

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>About Page</h1>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{ subtitle }}</h2>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      subtitle: 'この文書はNuxt.jsの基本的な使用方法について説明しています。',
+    }
+  },
   layout: 'original',
 }
 </script>
