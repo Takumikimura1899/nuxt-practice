7d3d7a2725882928afae23b5ab7e6ab231a55255
Takumikimura1899 <acmiran189926@gmail.com>
2022-07-24 05:56:12 +0900

    ルーティング設定

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,6 +3,7 @@
     <nav>
       <nuxt-link to="/" no-prefetch>Home</nuxt-link>
       <nuxt-link to="/about" no-prefetch>About</nuxt-link>
+      <nuxt-link :to="{ name: 'users-list' }">User LIst</nuxt-link>
     </nav>
   </div>
 </template>
