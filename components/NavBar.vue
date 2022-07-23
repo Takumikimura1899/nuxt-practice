236c281116da01a10bdba059cb9a706143508495
Takumikimura1899 <acmiran189926@gmail.com>
2022-07-23 20:49:32 +0900

    code spliting

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <nav>
-      <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/" no-prefetch>Home</nuxt-link>
+      <nuxt-link to="/about" no-prefetch>About</nuxt-link>
     </nav>
   </div>
 </template>
